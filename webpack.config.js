1087ef9a2b3500c29331e45c7c0def8d97b9e5f5
Amir Dadipoor <amirdadipoor>
2025-04-11 14:01:43 +0330

    build first requirements

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
     mode: "production",
@@ -9,5 +12,34 @@ module.exports = {
         path : path.resolve(__dirname,'dist'),
         filename: "[name].[contenthash].js",
     },
+
+    devServer : {
+        static: path.resolve(__dirname, "dist"), // Serve files from "dist"
+        port: 8989, // Choose any port
+        open: true, // Automatically open the browser
+        compress: true, // Enable gzip compression
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Trello App",
+            template: "./src/index.html",
+            filename: "index.html",
+            minify: false,
+        }),
+        new MiniCssExtractPlugin({
+            filename:  '[name].[contenthash].css'
+        }),
+        new CleanWebpackPlugin(),
+    ]
 
 }
