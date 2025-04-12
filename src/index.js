21f0c62f194b4a0a66c7bb1f538c745330f02075
Amir Dadipoor <amirdadipoor>
2025-04-12 22:47:06 +0330

    create list section Items

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import navbar from "./components/navbar"
 import sidebar from "./components/sidebar"
+import list from "./components/list-section"
 import "./style.css"
+import "./TaskFlow.css"
 
 
 let bodyClassList = ["p-5" ,"bg-white","dark:bg-gray-900","antialiased"]
 document.body.classList.add(...bodyClassList);
 document.body.appendChild(navbar.render());
 document.body.innerHTML += sidebar.render();
+document.body.innerHTML += list.render();
 
 console.log("123")
